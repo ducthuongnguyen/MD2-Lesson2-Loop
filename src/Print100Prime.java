147ecf1a829af195831c4b1a839667c34d19d641
ducthuongnguyen <ducthuong.nguyen241@gmail.com>
2022-03-23 08:15:50 +0700

    update 2

--- src/Print100Prime.java
+++ src/Print100Prime.java
@@ -1,7 +1,7 @@
 public class Print100Prime {
     public static void main(String[] args) {
         int count = 0;
-        for (int i = 2; count <100 ; i++) {
+        for (int i = 2; count <=100 ; i++) {
             boolean check = true;
             for (int j = 2; j <i ; j++) {
                 if (i%j==0){
